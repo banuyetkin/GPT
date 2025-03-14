c542b5859e277698149222ba6d498eeb316f73a1
Banu Ekren <banu.yetkinekren@cranfield.ac.uk>
2025-03-14 13:07:33 +0000

    Practically added endpoints clearly

--- backend.py
+++ backend.py
@@ -1,5 +1,17 @@
-from fastapi import FastAPI
-app = FastAPI()
+
+
+from fastapi import FastAPI, File, UploadFile
+import pandas as pd
+from gurobipy import *
 
 app = FastAPI()
-app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Backend Running"}
+
+@app.post("/solve/")
+async def solve(file: UploadFile = File(...)):
+    df = pd.read_excel(file.file)
+    # Your optimization
+
